e5b8000ea3a3801fa9d72c8163f4df8f9096f98e
Adam <apalm112@gmail.com>
2018-07-01 14:35:19 -0400

    Changed the title property to Library Manager to match project rubric.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Project #10' });
+  res.render('index', { title: 'Library Manager' });
 });
 
 module.exports = router;
