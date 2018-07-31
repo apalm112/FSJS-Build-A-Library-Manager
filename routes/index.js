b2358ffb077ce33d257c151fbb5b143e591a39d4
Adam <apalm112@gmail.com>
2018-07-31 00:12:44 -0400

    Added a #{title} property to index.js, changed file extensions from html to pug in nav.pug & added comment to layout.pug

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
 	res.render('layout', { title: 'Library Manager' });
 });
 
+router.get('/book/all_books.pug', function(req, res, next) {
+	res.render('all_books.pug');
+});
+
 module.exports = router;
