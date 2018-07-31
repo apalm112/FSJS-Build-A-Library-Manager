6c4e0f4dd8ff36005e944b98160c60c45ec68027
Adam <apalm112@gmail.com>
2018-07-30 23:10:03 -0400

    Changed the db source in config.json, fixed typo in migrations/loan.js, created the models for the project via sequelize cli.

---
+++ models/book.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Book = sequelize.define('Book', {
+    id: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    author: DataTypes.TEXT,
+    genre: DataTypes.STRING,
+    first_published: DataTypes.INTEGER
+  }, {});
+  Book.associate = function(models) {
+    // associations can be defined here
+  };
+  return Book;
+};
