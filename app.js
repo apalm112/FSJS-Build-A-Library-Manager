df49e3f79853a679a8a29c201a940f1b0355bcde
Adam <apalm112@gmail.com>
2018-08-08 11:37:47 -0400

    Changed the route names to have a more explicit naming convention, added cookie-parser & body-parser modules

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var createError = require('http-errors');
+var methodOverride = require('method-override');
 
-var mainRouter = require('./routes');
-// var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
+var library = require('./routes/library');
 
 var app = express();
 
@@ -15,14 +17,17 @@ const port = process.env.PORT || 4040;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', mainRouter);
-// app.use('/users', usersRouter);
+app.use('/', routes);
+app.use('/library', library);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
