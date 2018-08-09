951c6301fc1732370c0af2ea7845c0ed9618950d
Adam <apalm112@gmail.com>
2018-08-09 10:01:50 -0400

    Added validation code for the loans & patrons models.

--- models/patrons.js
+++ models/patrons.js
@@ -7,21 +7,51 @@ module.exports = (sequelize, DataTypes) => {
 		},
 		first_name: {
 			type: DataTypes.STRING,
+			validate: {
+				notEmpty: {
+					msg: 'Patron First Name is required!'
+				}
+			}
 		},
 		last_name: {
 			type: DataTypes.STRING,
+			validate: {
+				notEmpty: {
+					msg: 'Patron Last Name is required!'
+				}
+			}
 		},
 		address: {
 			type: DataTypes.STRING,
+			validate: {
+				notEmpty: {
+					msg: 'Patron address is required!'
+				}
+			}
 		},
 		email: {
 			type: DataTypes.STRING,
+			validate: {
+				notEmpty: {
+					msg: 'Patron Email is required!'
+				}
+			}
 		},
 		library_id: {
 			type: DataTypes.STRING,
+			validate: {
+				notEmpty: {
+					msg: 'Patron Library ID is required!'
+				}
+			}
 		},
 		zip_code: {
 			type: DataTypes.INTEGER,
+			validate: {
+				notEmpty: {
+					msg: 'Patron Zip Code is required!'
+				}
+			}
 		},
 	}, {});
 	patrons.associate = function(models) {
